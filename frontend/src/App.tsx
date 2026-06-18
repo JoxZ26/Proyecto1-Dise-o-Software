4bfc78b9ee22b4697e532ccf96fe420adad6eb55
JoxZ26 <jandresmurillo2605@gmail.com}>
2026-06-17 22:30:11 -0600

    Adelantos del frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,36 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
-import MenuPage from './pages/MenuPage';
+import RegisterPage from './pages/RegisterPage';
+import HomePage from './pages/HomePage';
+import BuscarGymsPage from './pages/BuscarGymsPage';
+import PerfilPage from './pages/PerfilPage';
+import AppLayout from './components/AppLayout';
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <MenuPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/registro" element={<RegisterPage />} />
+
+                {/* Rutas protegidas que comparten el navbar */}
+                <Route
+                    element={
+                        <ProtectedRoute>
+                            <AppLayout />
+                        </ProtectedRoute>
+                    }
+                >
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/buscar" element={<BuscarGymsPage />} />
+                    <Route path="/perfil" element={<PerfilPage />} />
+                </Route>
+
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
